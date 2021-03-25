b4173955ab79576e919890c6c82880873bc1ef2e
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-25 11:48:23 -0400

    add font style

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
 
   //TYPED JS
   var typed = new Typed(".typed", {
-    strings: ["Frontend Web Developer.", "Connoisseur of Food.", "Student."],
-    typeSpeed: 70,
+    strings: ["Frontend Web Developer.", "Food Connoisseur.", "Philomath (/ˈfɪləmæθ/) a lover of learning."],
+    typeSpeed: 100,
     loop: true,
     startDelay: 1000,
     showCursor: false,
