edb783329d51d7da583e39809dc22a5df3114623
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-25 14:26:08 -0400

    add owl carousel markup

--- js/script.js
+++ js/script.js
@@ -8,10 +8,32 @@ $(document).ready(function () {
 
   //TYPED JS
   var typed = new Typed(".typed", {
-    strings: ["Frontend Web Developer.", "Food Connoisseur.", "Philomath (/ˈfɪləmæθ/) a lover of learning."],
+    strings: [
+      "Frontend Web Developer.",
+      "Food Connoisseur.",
+      "Philomath (/ˈfɪləmæθ/) a lover of learning.",
+    ],
     typeSpeed: 100,
     loop: true,
     startDelay: 1000,
     showCursor: false,
+  });
+
+  //OWL CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
   });
 });
