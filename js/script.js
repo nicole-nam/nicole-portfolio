e1e934cd9e2a8e1f06cd4942241d63174260b9a7
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-25 11:27:46 -0400

    add typed js for title sub

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
+  // SUPERSLIDES
   $("#slides").superslides({
     animation: "fade",
     play: 5000,
     pagination: false,
+  });
+
+  //TYPED JS
+  var typed = new Typed(".typed", {
+    strings: ["Frontend Web Developer.", "Connoisseur of Food.", "Student."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
   });
 });
